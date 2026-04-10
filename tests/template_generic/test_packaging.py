ec6c7c8dd3f06ec438ba1faa075a00184ebee6ff
root <root@DESKTOP-VRJL30E.localdomain>
2026-04-09 20:47:42 -0400

    Fixed missing type annotation

--- tests/template_generic/test_packaging.py
+++ tests/template_generic/test_packaging.py
@@ -1,6 +1,6 @@
 import template_generic
 
 
-def test_version():
+def test_version() -> None:
     """Check to see that we can get the package version"""
     assert template_generic.__version__ is not None
